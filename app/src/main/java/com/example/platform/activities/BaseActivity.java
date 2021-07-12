25fc16a9c5e966c466c6f546b08d9f48f755e931
Stephon Ricks <stephon.ricks.01@gmail.com>
2021-07-12 16:25:04 -0500

    Increased app flow to include navigation flow - Added necessary fragments to do so

--- app/src/main/java/com/example/platform/activities/BaseActivity.java
+++ app/src/main/java/com/example/platform/activities/BaseActivity.java
@@ -1,16 +1,70 @@
 package com.example.platform.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.platform.R;
+import com.example.platform.fragments.CatalogFragment;
+import com.example.platform.fragments.ChatsFragment;
+import com.example.platform.fragments.CommunitiesFragment;
+import com.example.platform.fragments.HomeFragment;
+import com.example.platform.fragments.InboxFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BaseActivity extends AppCompatActivity {
+
+    private static final String TAG = "BaseActivity";
+    BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_communities:e:
+                        fragment = new CommunitiesFragment();
+                        Log.i(TAG, "Heading to Communities Fragment");
+                        break;
+                    case R.id.action_catalog:
+                        fragment = new CatalogFragment();
+                        Log.i(TAG, "Heading to Catalog Fragment");
+                        break;
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        Log.i(TAG, "Heading to Home Fragment");
+                        break;
+                        // TODO
+                    case R.id.action_chats:
+                        Log.i(TAG, "Heading to Chats Fragment");
+                        fragment = new ChatsFragment();
+                        break;
+                    case R.id.action_inbox:
+                        Log.i(TAG, "Heading to Inbix Fragment");
+                        fragment = new InboxFragment();
+                        break;
+                    default:
+                        fragment = new HomeFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection - only for Home
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
--- app/src/main/java/com/example/platform/activities/LoginActivity.java
+++ app/src/main/java/com/example/platform/activities/LoginActivity.java
@@ -2,15 +2,31 @@ package com.example.platform.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.platform.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin_Login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, BaseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
