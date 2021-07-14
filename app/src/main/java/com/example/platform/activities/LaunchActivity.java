4bfd344e00bf17c66e1788d2c1bd65e57d508055
Stephon Ricks <stephon.ricks.01@gmail.com>
2021-07-14 10:36:24 -0500

    User login persists - They are taken straight to home

--- app/src/main/java/com/example/platform/activities/LaunchActivity.java
+++ app/src/main/java/com/example/platform/activities/LaunchActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.platform.R;
+import com.parse.ParseUser;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -19,6 +21,11 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        // Checking if user is alreaady logged into the app
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
 
         btnSignup = findViewById(R.id.btnSignup_Launch);
         btnLogin = findViewById(R.id.btnLogin_Launch);
@@ -38,5 +45,13 @@ public class LaunchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void goMainActivity() {
+        Log.i(TAG, "Removing past activities from the stack to prevent user from going back");
+        Intent intent = new Intent(LaunchActivity.this, BaseActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
