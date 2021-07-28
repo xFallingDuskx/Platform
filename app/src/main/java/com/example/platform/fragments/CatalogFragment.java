5a83aa768c07dbc5fa854551e17ebc3b04c328ce
Stephon Ricks <stephon.ricks.01@gmail.com>
2021-07-27 20:44:59 -0500

    Affixed toolbar to other main fragments

--- app/src/main/java/com/example/platform/fragments/CatalogFragment.java
+++ app/src/main/java/com/example/platform/fragments/CatalogFragment.java
@@ -1,20 +1,40 @@
 package com.example.platform.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.platform.R;
+import com.example.platform.activities.ProfileActivity;
+import com.example.platform.activities.SearchActivity;
+import com.example.platform.adapters.HomeFragmentsAdapter;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
 public class CatalogFragment extends Fragment {
+
+    public static final String TAG = "CatalogFragment";
+    private ImageView ivProfile;
 
     public CatalogFragment() {
         // Required empty public constructor
@@ -26,9 +46,72 @@ public class CatalogFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Alert view that menu items exist
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_catalog, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.toolbar_home, menu);
+
+        // Allow user to search Titles
+        MenuItem searchItem = menu.findItem(R.id.miSearch);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Change appearance of EditText for the Search View
+        EditText searchEditText = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchEditText.setTextColor(getResources().getColor(R.color.white));
+        searchEditText.setHint("Search TV Shows and Movies");
+        searchEditText.setHintTextColor(getResources().getColor(R.color.grey_light));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Search for Titles according to the query
+                searchTitles(query);
+                // Reset the Search View and return to normal toolbar
+                searchView.clearFocus();
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+                searchItem.collapseActionView();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void searchTitles(String query) {
+        Intent intent = new Intent(getContext(), SearchActivity.class);
+        intent.putExtra("query", query);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Setting up the toolbar
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar_Catalog);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Take user to their profile
+        ivProfile = getActivity().findViewById(R.id.ivProfileIcon_Catalog);
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/platform/fragments/CommunitiesFragment.java
+++ app/src/main/java/com/example/platform/fragments/CommunitiesFragment.java
@@ -1,20 +1,38 @@
 package com.example.platform.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.platform.R;
+import com.example.platform.activities.ProfileActivity;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
 public class CommunitiesFragment extends Fragment {
+
+    public static final String TAG = "CommunitiesFragment";
+    private ImageView ivProfile;
 
     public CommunitiesFragment() {
         // Required empty public constructor
@@ -26,9 +44,37 @@ public class CommunitiesFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Alert view that menu items exist
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_communities, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.toolbar_communities, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Setting up the toolbar
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar_Communities);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Take user to their profile
+        ivProfile = getActivity().findViewById(R.id.ivProfileIcon_Communities);
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
