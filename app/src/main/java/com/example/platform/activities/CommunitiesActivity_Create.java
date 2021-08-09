de3bbbbf2c2aa94b93842f58cbd2b91a240dd951
Stephon Ricks <stephon.ricks.01@gmail.com>
2021-08-09 17:47:19 -0500

    Communities - onTrack to allowing user to create community - Issue w/ genre display

--- app/src/main/java/com/example/platform/activities/CommunitiesActivity_Create.java
+++ app/src/main/java/com/example/platform/activities/CommunitiesActivity_Create.java
@@ -2,17 +2,291 @@ package com.example.platform.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.platform.R;
+import com.example.platform.models.Community;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class CommunitiesActivity_Create extends AppCompatActivity {
 
     public static final String TAG = "CommunitiesActivity_Create";
+    Context context;
+    EditText etName;
+    EditText etKeywords;
+    EditText etDescription;
+    Button btnCreate;
+
+    TextView tvGenreDisplay;
+    RelativeLayout rlGenreSelection, rlGenreSpace;
+    boolean genresVisible = false;
+    ListView lvGenre;
+    List<String> genresSelected;
+    
+    SweetAlertDialog sweetAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communities_create);
+        context = getApplicationContext();
+
+        etName = findViewById(R.id.etNameInput_Communities);
+        tvGenreDisplay = findViewById(R.id.tvGenreDisplay);
+        lvGenre = findViewById(R.id.lvGenre);
+        rlGenreSelection = findViewById(R.id.rlGenre_Communities);
+        rlGenreSpace = findViewById(R.id.rlGenreSpace);
+        etKeywords = findViewById(R.id.etKeywordsInput_Communities);
+        etDescription = findViewById(R.id.etDescriptionInput_Communities);
+        btnCreate = findViewById(R.id.btnCreate_Communities);
+
+        // Allow users to select from a list of genres
+        setupGenreSelection();
+
+        // Create community on user submission
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: description must have a minimum of 100 words
+
+                showPopUp();
+            }
+        });
+    }
+
+    // Source: https://android--examples.blogspot.com/2016/03/android-listview-multiple-selection.html
+    // Source: https://www.codegrepper.com/code-examples/java/set+height+of+layout+programmatically+android
+    private void setupGenreSelection() {
+        tvGenreDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (! genresVisible) {
+                    openGenreSelection();
+                } else {
+                    closeGenreSelection();
+                }
+            }
+        });
+
+
+        List<String> genreOptions = Arrays.asList(
+                "Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama",
+                "Family", "Fantasy", "History", "Horror", "Music", "Reality", "Romance",
+                "Science Fiction", "Soap", "Talk", "Thriller", "War", "Western"
+        );
+
+        // Initialize a new ArrayAdapter
+        // Change context to 'this' if there are issues
+        ArrayAdapter<String> adapter = new ArrayAdapter(context, android.R.layout.simple_list_item_multiple_choice, genreOptions);
+        lvGenre.setAdapter(adapter);
+        lvGenre.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                SparseBooleanArray clickedItemPositions = lvGenre.getCheckedItemPositions();
+                for(int index = 0; index<clickedItemPositions.size(); index++){
+                    // Get the checked status of the current item
+                    boolean checked = clickedItemPositions.valueAt(index);
+
+                    // If the item has been checked by the user
+                    if(checked){
+                        int key = clickedItemPositions.keyAt(index);
+                        String genre = (String) lvGenre.getItemAtPosition(key);
+
+                        if (index == 0) {
+                            tvGenreDisplay.setText(genre);
+                        } else {
+                            tvGenreDisplay.setText(tvGenreDisplay.getText() + ", " + genre);
+                        }
+                        genresSelected.add(genre);
+                    }
+                }
+            }
+        });
+    }
+
+    public void showPopUp() {
+        sweetAlertDialog = new SweetAlertDialog(context, SweetAlertDialog.NORMAL_TYPE);
+        sweetAlertDialog.setTitleText("Are you sure?")
+                .setContentText("Make sure all information regarding the community has been entered properly to avoid issues later on.")
+
+                .setConfirmButton("Yes", new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
+                        Log.i(TAG, "User is onTrack to delete the comment");
+                        // Change SweetAlert to loading type
+                        sweetAlertDialog.changeAlertType(SweetAlertDialog.PROGRESS_TYPE);
+                        sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#171717"));
+                        sweetAlertDialog.setTitleText("Create...");
+                        sweetAlertDialog.setContentText("Your community is being created");
+                        sweetAlertDialog.setCancelable(false);
+
+                        createCommunity();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                sweetAlertDialog.setTitleText("Create!")
+                                        .setContentText("Your Platform community has been successfully created!")
+                                        .setConfirmText("OK")
+                                        .setConfirmClickListener(null)
+                                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                            }
+                        }, 4000);
+                    }
+                })
+                .show();
+    }
+
+
+    public void createCommunity() {
+        // Get keywords
+        String[] keywordsArray = etKeywords.getText().toString().split(", ");
+        List<String> keywords = new ArrayList<>();
+        for (int i = 0; i < keywordsArray.length; i++) {
+            String keyword = keywordsArray[i].toLowerCase();
+            keywords.add(keyword);
+        }
+
+        ParseObject community = new ParseObject("Community");
+        community.put(Community.KEY_NAME, etName.getText().toString());
+        community.put(Community.KEY_DESCRIPTION, etDescription.getText().toString());
+        community.put(Community.KEY_CREATOR, ParseUser.getCurrentUser().getUsername());
+        community.put(Community.KEY_MEMBERS, new JSONArray());
+        community.put(Community.KEY_GENRES, genresSelected);
+        community.put(Community.KEY_KEYWORDS, keywords);
+
+        // Saves the new object.
+        // Notice that the SaveCallback is totally optional!
+        community.saveInBackground(e -> {
+            if (e==null){
+                Log.i(TAG, "Community was saved successfully");
+                sweetAlertDialog.setTitleText("Create!")
+                        .setContentText("Your Platform community has been successfully created!")
+                        .setConfirmText("OK")
+                        .setConfirmClickListener(null)
+                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+            }else{
+                Log.d(TAG, "Issue saving community /Error: " + e.getMessage());
+                sweetAlertDialog.setTitleText("Sorry!")
+                        .setContentText("There was an issue creating your community")
+                        .setConfirmText("OK")
+                        .setConfirmClickListener(null)
+                        .changeAlertType(SweetAlertDialog.ERROR_TYPE);
+            }
+        });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        // Community genres - outside of
+        Rect genreRect = new Rect();
+        rlGenreSpace.getGlobalVisibleRect(genreRect);
+        if (!genreRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+            Log.i(TAG, "Outside of genre space");
+            if (genresVisible) {
+                closeGenreSelection();
+            }
+        }
+
+        // Community genres - inside of
+        Rect genresRect = new Rect();
+        rlGenreSelection.getGlobalVisibleRect(genresRect);
+        if (genresRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+            Log.i(TAG, "Inside of genre space");
+            if (!genresVisible) {
+                openGenreSelection();
+            }
+            etName.clearFocus();
+            etKeywords.clearFocus();
+            etDescription.clearFocus();
+            // Close the keyboard
+            // Source: https://gist.github.com/lopspower/6e20680305ddfcb11e1e
+            InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+            //Find the currently focused view, so we can grab the correct window token from it.
+            View view = findViewById(android.R.id.content).getRootView();
+            //If no view currently has focus, create a new one, just so we can grab a window token from it
+            if (view == null) {
+                view = new View(context);
+            }
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        // Community name input
+        Rect nameRect = new Rect();
+        etName.getGlobalVisibleRect(nameRect);
+        if (nameRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+            Log.i(TAG, "Inside of name space");
+            etName.requestFocus();
+            if (genresVisible) {
+                closeGenreSelection();
+            }
+        }
+
+        // Community keywords input
+        Rect keywordsRect = new Rect();
+        etKeywords.getGlobalVisibleRect(keywordsRect);
+        if (keywordsRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+            Log.i(TAG, "Inside of keywords space");
+            etKeywords.requestFocus();
+            if (genresVisible) {
+                closeGenreSelection();
+            }
+        }
+
+        // Community description input
+        Rect descriptionRect = new Rect();
+        etDescription.getGlobalVisibleRect(descriptionRect);
+        if (descriptionRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+            Log.i(TAG, "Inside of description space");
+            etDescription.requestFocus();
+            if (genresVisible) {
+                closeGenreSelection();
+            }
+        }
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public void closeGenreSelection() {
+        ViewGroup.LayoutParams params = rlGenreSelection.getLayoutParams();
+        params.height = 115;
+        rlGenreSelection.setLayoutParams(params);
+        lvGenre.setVisibility(View.GONE);
+        genresVisible = false;
+    }
+
+    public void openGenreSelection() {
+        ViewGroup.LayoutParams params = rlGenreSelection.getLayoutParams();
+        params.height = 600;
+        rlGenreSelection.setLayoutParams(params);
+        lvGenre.setVisibility(View.VISIBLE);
+        genresVisible = true;
     }
 }
